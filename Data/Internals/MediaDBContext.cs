5002efd036c73f8602f6a334a706bc25a0a481f4
Rasmus Olsson <S192312@student.hb.se>
2023-10-13 20:16:39 +0200

    Added the test GUI

--- Data/Internals/MediaDBContext.cs
+++ Data/Internals/MediaDBContext.cs
@@ -1,7 +1,18 @@
-﻿namespace DataLayer.Internals
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DataLayer.Internals
 {
-    public class MediaDBContext
+    public class MediaDBContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            if (!optionBuilder.IsConfigured)
+            {
+                optionBuilder.UseSqlServer("Data Source=(LocalDb)\\MSSQLLocalDB;Database=OlssonMediaApp");
+                base.OnConfiguring(optionBuilder);
+            }
+        }
 
     }
 }
